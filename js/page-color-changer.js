8440b01a4a86f0b8d174e7aa8a3bef7c19a43ff4
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-02-07 11:47:22 -0500

    updated script

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -6,17 +6,8 @@ let sliders = document.getElementsByTagName('input');
 let rgb = [0,0,0];
 
 // Build one or more event handlers to call a function when the user changes the value of one of the range sliders (hint - look up the code for the weather page where we built a slider for the wind speed).
-for (let i=0; i < sliders.length; i++) {
-    sliders[i].onchange = function() {
-        let whichSlider = this.getAttribute('id');
-        let sliderValue = this.value;
-        newRgb = changeRgb(whichSlider, sliderValue);
-        let newCss = writeCSS(newRgb);
-    }
-}
 
-// Build one or more functions that change the background-color property value of the page body (or HTML element), based on the values of the three range sliders (hint - we changed the background-color of the page in a prior lesson).
-function changeRgb (color, value) {
+function changeRgb (color,value) {
     switch (color) {
         case (red):
             rgb[0] = parseInt (value);
@@ -33,18 +24,26 @@ function changeRgb (color, value) {
         return rgb;
     }
 
-function writeCss (updateRgb) {
+function writeCSS () {
     let newColor = "rgb(";
-    let i  = 0;
-    while ( i < newRgb.length - 1) (
-        newColor += newRgb[i] + ","
+    let i = 0;
+    while ( i < newRgb.length - 1) {
+        newColor += newRgb[i] + ",";
         i++;
-    )
+    }
     newColor += newRgb [i] + ",";
     return newColor;
 }
 newColor = `rgb(255,0,0), rgb(0,255,0), rgb(0,0,255),`
 console.log(newColor);
 
-
-
+// Build one or more functions that change the background-color property value of the page body (or HTML element), based on the values of the three range sliders (hint - we changed the background-color of the page in a prior lesson).
+for (let i=0; i < sliders.length; i++) {
+    sliders[i].onchange = function() {
+        let whichSlider = this.getAttribute('id');
+        let sliderValue = this.value;
+        newRgb = changeRgb(whichSlider, sliderValue);
+        newCss = writeCSS(newRgb);
+        pageBg.style.backgroundColor = newCSS;
+    }
+}
